7e85bb45a315d36b54cc39b787f5d51f475ef85e
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-09-18 10:33:45 +0530

    Selenium Testing

---
+++ JavaDataStructure/src/main/java/com/tap/sarthak/demo/DataStructure/HashMapEg.java
@@ -0,0 +1,12 @@
+package com.tap.sarthak.demo.DataStructure;
+
+import java.util.HashMap;
+
+public class HashMapEg {
+    
+    HashMap<String,String> countrycaptial = new HashMap<String,String>();
+
+    // add elements in the HashMap
+
+    countrycaptial.put
+}
