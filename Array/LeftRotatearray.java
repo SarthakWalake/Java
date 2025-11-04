a95dbcd7da8e10147d2f14ca975a3ef343dba051
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-11-04 21:24:15 +0530

    Left Rotate Array by One

--- Array/LeftRotatearray.java
+++ Array/LeftRotatearray.java
@@ -14,6 +14,11 @@ public class LeftRotatearray {
 
         int temp = arr[0];
 
-        for(int i =1;)
+        for(int i =1;i<arr.length;i++)
+        {
+            arr[i-1] = arr[i];
+        }
+
+        arr[arr.length-1] = temp;
     }
 }
