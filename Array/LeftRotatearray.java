7590ed21317a863e40f9c6bcacb127c6761bb9a9
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-11-04 21:19:23 +0530

    Left Rotate Array by one

---
+++ Array/LeftRotatearray.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class LeftRotatearray {
+    public static void main(String [] args)
+    {
+        int arr[] = new int[5];
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the element of the Array");
+
+        for(int i =)
+    }
+}
