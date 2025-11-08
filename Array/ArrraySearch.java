1d5c34a86486c923a7e4394b9cdfac4c3988d2e4
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-11-08 23:33:58 +0530

    Array Search

---
+++ Array/ArrraySearch.java
@@ -0,0 +1,37 @@
+// To search the position of the array
+
+import java.util.*;
+
+public class ArrraySearch {
+    public static void main(String [] args)
+    {
+        int arr[] = new int[5];
+        int search;
+        boolean flag = false;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the elements of the Array");
+
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the element to be search");
+        search = sc.nextInt();
+
+        for(int i =0;i<arr.length;i++)
+        {
+            if(arr[i] == search)
+            {
+                flag = true;
+                break;
+            }
+
+            if(flag == true)
+            {
+                System.out.println(search+"Found ");
+            }
+        }
+    }
+}
