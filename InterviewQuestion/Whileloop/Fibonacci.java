bef24428e1fd609d5904d1d1f4d95fb3f010441b
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-11-10 16:17:09 +0530

    Fabonacci Series

---
+++ InterviewQuestion/Whileloop/Fibonacci.java
@@ -0,0 +1,25 @@
+// Fabonacci Series
+
+import java.util.*;
+
+public class Fibonacci {
+    public static void main(String [] args)
+    {
+        int n1 = 0;
+        int n2 = 1;
+        int n , n3;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        n = sc.nextInt();
+
+        System.out.print(" "+n1+" "+n2);
+        for(int i =2;i<n;i++)
+        {
+            n3 = n2 + n1;
+            System.out.print(" "+n3);
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+}
