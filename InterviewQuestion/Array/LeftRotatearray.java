e48efca278f83d73ed49c593e72ff66915fa9e34
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-11-10 21:45:38 +0530

    .

--- InterviewQuestion/Array/LeftRotatearray.java
+++ InterviewQuestion/Array/LeftRotatearray.java
@@ -20,5 +20,10 @@ public class LeftRotatearray {
         }
 
         arr[arr.length-1] = temp;
+
+        for(int i =0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
     }
 }
