77c9330cb84cbb28623076abb3e43fd8d54c046b
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-11-12 22:18:30 +0530

    Prime Number

--- InterviewQuestion/Whileloop/Prime.java
+++ InterviewQuestion/Whileloop/Prime.java
@@ -8,5 +8,11 @@ public class Prime {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number = ");
         num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
     }
 }
