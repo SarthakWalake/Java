209fb6270f6208f6e364daee64f1321e97c31004
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-11-13 22:11:06 +0530

    Prime check from list

--- InterviewQuestion/Whileloop/PrimeCheck.java
+++ InterviewQuestion/Whileloop/PrimeCheck.java
@@ -7,5 +7,25 @@ public class PrimeCheck {
     {
         int n = 50;
         boolean flag;
+
+        while(n<=100)
+        {
+           int  i = 2;
+            flag = false;
+
+            while(i < n)
+            {
+                if(n % i == 0)
+                {
+                    flag = true;
+                    break;
+                }
+                i = i + 1; 
+            }
+            if(flag == false)
+            {
+                System.out.println(n);
+            }
+        }
     }
 }
