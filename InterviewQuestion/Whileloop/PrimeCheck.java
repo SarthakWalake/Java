8aa315fb6796d0cd4ff89d45cb932031b9272358
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-11-13 22:07:52 +0530

    Prime Check From list

---
+++ InterviewQuestion/Whileloop/PrimeCheck.java
@@ -0,0 +1,11 @@
+// Prime number check form the list from 50 to 100
+
+import java.util.*;
+
+public class PrimeCheck {
+    public static void main(String [] args)
+    {
+        int n = 50;
+        boolean flag;
+    }
+}
