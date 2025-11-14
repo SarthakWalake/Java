bf5aba61d821a5c30283a2bb66602c4515264db2
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-11-14 21:51:28 +0530

    Spy Num

---
+++ InterviewQuestion/Whileloop/SpyNum.java
@@ -0,0 +1,13 @@
+// Check the SPY number eg 123  1+2+3 = 6  && 1*2*3 = 6
+
+import java.util.*;
+
+public class SpyNum {
+    public static void main(String [] args)
+    {
+        int n;
+        int rem = 0;
+        int sum = 0;
+        int multi = 1;
+    }
+}
