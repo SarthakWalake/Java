125028cca08b323a9a05207fd6db0b94186375a2
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-11-17 23:30:45 +0530

    pattern Code

--- InterviewQuestion/Pattern/Pattern4.java
+++ InterviewQuestion/Pattern/Pattern4.java
@@ -10,7 +10,7 @@ public class Pattern4 {
     {
     for(int i = 5; i >= 1;i--)
         {
-        for(int j = i;j <= 5;j++)
+        for(int j = i;j <= 1;j--)
         {
             System.out.print(j+" ");
         }
