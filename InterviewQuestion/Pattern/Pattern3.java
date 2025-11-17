663490437b443f3cbfe89987b73d5a35095c5aa0
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-11-17 23:28:59 +0530

    Pattern code

--- InterviewQuestion/Pattern/Pattern3.java
+++ InterviewQuestion/Pattern/Pattern3.java
@@ -9,11 +9,11 @@
 public class Pattern3 {
     public static void main(String [] args)
     {
-        for(int i = 1;i <= 5;i++)
+        for(int i = 1;i <= 5;i--)
         {
-             for(int j = 1;j <= i;j++)
+             for(int j = 1;j >= i;j--)
             {
-                System.out.print(i+" ");
+                System.out.print(j+" ");
             }
             System.out.println();
         }
