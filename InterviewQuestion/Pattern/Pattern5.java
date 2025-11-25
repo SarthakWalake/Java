5d2798566134ad64c880db8cc99d2e22f40d42a9
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-11-25 22:23:46 +0530

    Print Follwoing pattern

--- InterviewQuestion/Pattern/Pattern5.java
+++ InterviewQuestion/Pattern/Pattern5.java
@@ -9,6 +9,14 @@
 public class Pattern5 {
     public static void main(String [] args)
     {
-        
+        int i,j,k,l;
+
+        for(i = 1;i<=4;i++)
+        {
+            for(k=i;k<=5;k++)
+            {
+                
+            }
+        }
     }
 }
