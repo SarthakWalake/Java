eec9c6dbbeb8bc017aba77744f0073e00d11e2b3
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-11-25 22:29:42 +0530

    Print Follwoing pattern

--- InterviewQuestion/Pattern/Pattern5.java
+++ InterviewQuestion/Pattern/Pattern5.java
@@ -15,17 +15,17 @@ public class Pattern5 {
         {
             for(k=i;k<=5;k++)
             {
-                System.out.println(" ");
+                System.out.print(" ");
             }
             for(j=1;j<i;j++)
             {
-                System.out.println(j+" ");
+                System.out.print(j+" ");
             }
             for(l=j;l>=1;l--)
             {
-                System.out.println(l+" ");
+                System.out.print(l+" ");
             }
-            
+            System.out.println();
         }
     }
 }
